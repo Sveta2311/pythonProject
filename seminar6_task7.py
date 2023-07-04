999b41ee4f87526b33e3523626d4d06caf2ff97a
Sveta <margaritka2132@mail.ru>
2023-07-04 22:14:04 +0400

    git commit -a -m

--- seminar6_task7.py
+++ seminar6_task7.py
@@ -1,5 +1,5 @@
 # from seminar6 import calendar
 #
 # if __name__ == '__main__':
-#     print(calendar('12.12.2020'))
-#     print(calendar('29.02.2020'))
+#     print(calendar('12.12.2016'))
+#     print(calendar('29.02.2016'))
